92aac66d4db6334a2a3cd622204db7a5478a3ea4
Michael Wasilewski <michael_grimestad94@hotmail.com>
2023-02-23 13:05:37 +0100

    add navbar links, some styling

--- lesson-task-day-2/src/App.js
+++ lesson-task-day-2/src/App.js
@@ -1,15 +1,30 @@
 import './App.css';
-import DefaultProfilePic from './components/DefaultProfilePic';
-import ProfileName from './components/ProfileName';
+import React from 'react';
+// import DefaultProfilePic from './components/DefaultProfilePic';
+// import ProfileName from './components/ProfileName';
+import Home from './routes/Home';
+import About from './routes/About';
+import Project from './routes/Project';
+import Contact from './routes/Contact';
+
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <ProfileName name="Michael" />
-      <DefaultProfilePic altText="Nice" />
-      <ProfileName name="Sander" />
-      <DefaultProfilePic altText="Shmice" />
-    </div>
+    // <div className="App">
+    //   <ProfileName name="Michael" />
+    //   <DefaultProfilePic altText="Nice" />
+    //   <ProfileName name="Sander" />
+    //   <DefaultProfilePic altText="Shmice" />
+    // </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/project" element={<Project />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ lesson-task-day-2/src/components/Navbar.js
@@ -0,0 +1,29 @@
+import './NavbarStyles.css';
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+export const Navbar = () => {
+  return (
+    <div className="header">
+      <Link to="/">
+        <h1>Portfolio</h1>
+      </Link>
+      <ul className="nav-menu">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/project">Projects</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
